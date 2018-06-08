d71aed48add665d883b97564ddf65257337e3383
Cyril ferte <cyril.ferte-sc@airliquide.com>
2018-06-08 08:08:40 +0200

    download table

--- src/app/ui/table/table.component.ts
+++ src/app/ui/table/table.component.ts
@@ -8,6 +8,8 @@ import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import { Router } from '@angular/router';
 
+ import 'rxjs/Rx' ;
+
 
 @Component({
   selector: 'ui-table',
@@ -22,7 +24,7 @@ export class TableComponent implements OnInit {
   filteredStates: Observable<any[]>;
   @Input() public table;
   @Input() public searchValve: Boolean = false;
-
+  @Input() public downloadData: Boolean = false;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name', 'type', 'mode', 'position', 'last_purge_date', 'last_error'];
@@ -31,6 +33,7 @@ export class TableComponent implements OnInit {
   }
   ngOnInit() {
     if (this.table){
+
       this.dataSource = new TableDataSource(this.paginator, this.sort, this.valveService, this.table);
       this.stateCtrl = new FormControl();
       this.filteredStates = this.stateCtrl.valueChanges
@@ -43,6 +46,8 @@ export class TableComponent implements OnInit {
 
   }
 
+
+
   public filterStates(name: string) {
     return this.table.filter(table =>
       table.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
@@ -50,5 +55,29 @@ export class TableComponent implements OnInit {
 
     public redirectToValve(id: string){
       console.log(id)
+    }
+
+    public onDownloadData(){
+      console.log(this.table)
+      var blob = new Blob([this.convertToCSV(this.table)], { type: 'text/csv' });
+    var url= window.URL.createObjectURL(blob);
+    window.open(url);
+    }
+     convertToCSV(objArray) {
+        var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+        var str = '';
+
+        for (var i = 0; i < array.length; i++) {
+            var line = '';
+            for (var index in array[i]) {
+                if (line != '') line += ','
+
+                line += array[i][index];
+            }
+
+            str += line + '\r\n';
+        }
+
+        return str;
     }
 }
